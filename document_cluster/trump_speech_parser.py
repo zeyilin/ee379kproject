9d02b4d18d0683b341faaa3ea4e48ce3a9518655
Taylor Schmidt <tschmidt@sparkcognition.com>
2017-12-05 16:08:37 -0600

    added speeches

--- document_cluster/trump_speech_parser.py
+++ document_cluster/trump_speech_parser.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
 import requests
-import csv
 from nltk import tokenize
 from bs4 import BeautifulSoup
 
+# Each one of these is an speech
 pids = [ 110306, 116597, 117813, 117775, 117790, 117815, 117791, 117935, 118047, 123199, 119744, 122534, 123515, 119503, 119165, 119175, 123197, 119176, 123198, 119806, 119805, 119196, 119198, 119199, 119197, 119177, 119195, 119194, 119204, 119178, 119205, 119193, 119206, 119190, 119209, 119208, 119207, 119192, 119189, 119191, 119203, 119200, 119201, 119202, 119179, 119166, 119167, 119168, 119169, 119170, 119171, 123517, 119173, 119180, 119172, 119174, 119183, 123516, 119184, 119185, 119186, 119875, 119187, 119188, 123518, 123519, 119181, 119182, 122537, 122535, 123513, 123514, 122536 ]
 
 strs_to_delete = [u'—', u':', u'[]', u' – ', '...']
 
-
 all_text = ''
 
-for pid in pids[:3]:
+# Loop over all speeches
+for pid in pids:
     page = requests.get('http://www.presidency.ucsb.edu/ws/index.php?pid=%s' % pid)
     soup = BeautifulSoup(page.content, 'html.parser')
 
@@ -31,15 +31,22 @@ for pid in pids[:3]:
         elif t.__class__.__name__ == 'NavigableString': 
             all_text += t
         else:
-            print("FUCK I DIDN'T KNOW ABOUT THIS")
+            print("FUCK I DIDN'T EXPECT THIS TYPE")
         
+        # Just to make sure the words are separate
         all_text += ' '
 
 
+# Remove garbage
 for str_to_delete in strs_to_delete:
     all_text = all_text.replace(str_to_delete, ' ')
 
+# Split into sentences
 toks = tokenize.sent_tokenize(all_text)
 
-for tok in toks:
-    print(tok)
+with open('speeches.txt', 'w') as f:
+    for tok in toks:
+        line = tok + '\n'
+        line = line.encode('utf8')
+        f.write(line)
+    
